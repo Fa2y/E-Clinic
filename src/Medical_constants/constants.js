0ff21d48fab2c30209fdf5fcb4e672aaf94ade01
HoussemBenmammar <56603730+HoussemBenmammar@users.noreply.github.com>
2021-08-02 11:11:15 +0200

    Add files via upload

---
+++ src/Medical_constants/constants.js
@@ -0,0 +1,45 @@
+export const MedicalObjects= {
+     currencies : [
+        {
+          value: 'USD',
+          label: '$',
+        },
+        {
+          value: 'houssem',
+          label: 'houssem',
+        },
+        {
+          value: 'BTC',
+          label: '฿',
+        },
+        {
+          value: 'JPY',
+          label: '¥',
+        },
+      ],
+          tobaco_consumption : [
+        {
+          value: 'true',
+          label: 'Yes',
+        },
+        {  
+          value: 'false',
+          label: 'No',
+        },
+      ],
+        PRODUCT : [
+        {
+          value: 'smoking tobaco',
+          label: 'smoking tobaco',
+        },
+        {  
+          value: 'chewing tobaco',
+          label: 'chewing tobaco',
+        },
+        {  
+          value: 'injection tobaco',
+          label: 'injection tobaco',
+        },
+      ],
+ 
+}
