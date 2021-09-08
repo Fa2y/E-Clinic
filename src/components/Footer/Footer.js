2fb65258f033975f1e2d5d889c2f246531ffc062
Fa2y <a.merzoukbenselloua@esi-sba.dz>
2021-09-08 16:26:41 +0100

    Changed to our text instead of creative tim

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,12 @@
 /*eslint-disable*/
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
-import ListItem from "@material-ui/core/ListItem";
-import List from "@material-ui/core/List";
+import { makeStyles } from '@material-ui/core/styles';
+import ListItem from '@material-ui/core/ListItem';
+import List from '@material-ui/core/List';
 // core components
-import styles from "assets/jss/material-dashboard-react/components/footerStyle.js";
+import styles from 'assets/jss/material-dashboard-react/components/footerStyle.js';
 
 const useStyles = makeStyles(styles);
 
@@ -41,15 +41,11 @@ export default function Footer(props) {
         </div>
         <p className={classes.right}>
           <span>
-            &copy; {1900 + new Date().getYear()}{" "}
-            <a
-              href="https://www.creative-tim.com?ref=mdr-footer"
-              target="_blank"
-              className={classes.a}
-            >
-              Creative Tim
+            &copy; {1900 + new Date().getYear()}{' '}
+            <a href="/" target="_blank" className={classes.a}>
+              E-Clinic
             </a>
-            , made with love for a better web
+            Made With Love for ESI-SBA
           </span>
         </p>
       </div>
