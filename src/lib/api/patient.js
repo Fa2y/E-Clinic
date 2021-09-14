a1f169ddcda819c6b9010ac846c89eaceeddd6b2
Fa2y <a.merzoukbenselloua@esi-sba.dz>
2021-09-14 11:48:46 +0100

    Added Special Medical Exam create

--- src/lib/api/patient.js
+++ src/lib/api/patient.js
@@ -36,6 +36,51 @@ const patientAPI = {
       return error.response;
     }
   },
+  createMedicalExam: async (data) => {
+    const token = getToken();
+    try {
+      const response = await axios.post(
+        `${SERVER_URL}/api-medical/medical_exam/`,
+        data,
+        {
+          headers: {
+            Authorization: `token ${encodeURIComponent(token)}`,
+          },
+        },
+      );
+      return response;
+    } catch (error) {
+      return error.response;
+    }
+  },
+  addMedicalExamDetails: async (meid, part, data) => {
+    const token = getToken();
+    try {
+      const formData = new FormData();
+      if (part === 'paraclinical_exam') {
+        Object.entries(data).forEach(([key, value]) => {
+          formData.append(key, value);
+        });
+        formData.append('part', 'paraclinical_exam');
+      }
+      const response = await axios.post(
+        `${SERVER_URL}/api-medical/medical_exam/${meid}/create_detail/`,
+        part === 'paraclinical_exam' ? formData : { part, data },
+        {
+          headers: {
+            Authorization: `token ${encodeURIComponent(token)}`,
+            'Content-Type':
+              part === 'paraclinical_exam'
+                ? 'multipart/form-data'
+                : 'application/json',
+          },
+        },
+      );
+      return response;
+    } catch (error) {
+      return error.response;
+    }
+  },
 };
 
 export default patientAPI;
